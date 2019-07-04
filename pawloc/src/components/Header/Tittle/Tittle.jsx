f8280fa98e9da6c8c006d35d12185c9264250400
Yesenia <hernandezy1603@gmail.com>
2019-07-04 11:21:49 -0500

    ya mero

--- pawloc/src/components/Header/Tittle/Tittle.jsx
+++ pawloc/src/components/Header/Tittle/Tittle.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import  Link from 'react-router-dom/Link';
+import GoogleF from 'react-google-font-loader';
+
+
+import './tittle.css';
 
 //import FALTA ESTILO
 
 const Tittle = () => (
     <Link to="/">
-        <h1>LocPaw</h1>
+        <h1>
+            LocPaw
+        </h1>
     </Link>
 )
 
